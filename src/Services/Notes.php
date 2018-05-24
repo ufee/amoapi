24e16910a5d429a7d28a7f748b576a853f1cae7f
vldiark <vldiark@gmail.com>
2018-05-24 16:21:15 +0300

    Fix find method: rewrited

--- src/Services/Notes.php
+++ src/Services/Notes.php
@@ -16,7 +16,7 @@ class Notes extends \Ufee\Amo\Base\Services\LimitedList
 		$entity_model = '\Ufee\Amo\Models\Note',
 		$entity_collection = '\Ufee\Amo\Collections\NoteCollection',
 		$cache_time = false;
-	
+
     /**
      * Get notes by type
 	 * @param integer $id
@@ -36,5 +36,27 @@ class Notes extends \Ufee\Amo\Base\Services\LimitedList
 	{
 		return $this->list->where('type', 4)
 					->recursiveCall();
+	}
+
+    /**
+     * Get models by id
+	 * @param integer|array $id
+	 * @param integer $type note type
+	 * @return Model|Collection
+     */
+	public function find($id, $type = 4)
+	{
+		$result = $this->list->where('limit_rows', is_array($id) ? count($id) : 1)
+							 ->where('limit_offset', 0)
+							 ->where('type', $type)
+							 ->where('id', $id)
+							 ->call();
+		if (is_array($id)) {
+			return $result;
+		}
+		if (!$model = $result->get(0)) {
+			return null;
+		}
+		return $model;
 	}
 }
