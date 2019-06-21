54368858d2a7efdfd67f1ecfeccb64f119c7fb05
vldiark <vldiark@gmail.com>
2019-06-21 17:23:19 +0300

    Fix for empty user groups

--- src/Collections/UserGroupCollection.php
+++ src/Collections/UserGroupCollection.php
@@ -3,8 +3,23 @@
  * amoCRM User group model Collection class
  */
 namespace Ufee\Amo\Collections;
+use Ufee\Amo\Models\UserGroup;
 
 class UserGroupCollection extends \Ufee\Amo\Base\Collections\Collection
 {
-	
+	/**
+	 * Get group by id
+	 * @param $id - user group id
+	 * @return UserGroup
+	 */
+	public function byId($id)
+	{
+        if (!$group = $this->find('id', $id)->first()) {
+            $group = new UserGroup([
+                'id' => '',
+                'name' => ''
+            ]);
+        }
+        return $group;
+	}
 }
