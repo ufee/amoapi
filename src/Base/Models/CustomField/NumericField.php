1c19c6730c863339095807ee060d9aa3eec36c82
Ufee <vlad@ufee.ru>
2020-07-18 13:48:04 +0300

    Added floatval output

--- src/Base/Models/CustomField/NumericField.php
+++ src/Base/Models/CustomField/NumericField.php
@@ -6,5 +6,28 @@ namespace Ufee\Amo\Base\Models\CustomField;
 
 class NumericField extends EntityField
 {
+    /**
+     * Get cf value
+	 * @return mixed
+     */
+    public function getValue()
+    {
+		if (!isset($this->values[0])) {
+			return null;
+		}
+		return floatval($this->values[0]->value);
+	}
 
+    /**
+     * Get cf values
+	 * @return array
+     */
+    public function getValues()
+    {
+        $values = [];
+		foreach ($this->values as $setted) {
+            $values[]= floatval($setted->value);
+        }
+        return $values;
+    }
 }
